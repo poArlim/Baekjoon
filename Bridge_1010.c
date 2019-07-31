d4c7f5e07be32a736ffc3a874b0f30f964e520fb
poArlim <dkfhscjsgk96@gmail.com>
2019-07-31 09:57:51 +0900

    Update for July 31

--- Bridge_1010.c
+++ Bridge_1010.c
@@ -6,10 +6,12 @@ int main()
 {
     int T, N, M;
     scanf("%d", &T);
+    
     for(int i = 0; i < T; i++){
         scanf("%d %d", &N, &M);
         printf("%d\n", combination(N, M));
     }
+
     return 0;
 }
 
@@ -18,20 +20,21 @@ int combination(int a, int b){
     double denominator = 1;
     double result = 1;
     int i;
+    
     if(b - a <= a){
         for(i = 0; i < b - a; i++){
             nominator *= b - i;
             denominator *= b - a - i;
-            // result *= (nominator / denominator);
         }
     }
     else{
         for(i = 0; i < a; i++){
             nominator *= b - i;
             denominator *= a - i;
-            // result *= (nominator / denominator);
         }
     }
+    
     result = nominator / denominator;
+    
     return (int)result;
 }
