1878e21d84ffe0c1159c57aa2d1d884f4e8b9dd4
poArlim <dkfhscjsgk96@gmail.com>
2020-12-09 20:44:54 +0900

    solved Dec 9

---
+++ MakeOne_1463.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+vector<int> minCount;
+
+int MakeOne(int n){
+	if (n == 0) {
+		cout<<"0 could not be calculated."<<endl;
+		return 0;
+	}
+	if (n != -1) return minCount[n];
+	
+	int result = 1000001;
+
+	if (n % 3 == 0) result = min(result, MakeOne(n/3) + 1);
+	if (n % 2 == 0) result = min(result, MakeOne(n/2) + 1);
+	result = min(result, MakeOne(n-1) + 1);
+
+	minCount[n] = result;
+
+	return result;
+}
+
+
+int main(){
+	int N;
+
+	cin>>N;
+	minCount.resize(N+1, -1);
+	
+	minCount[1] = 0;
+	cout<<MakeOne(N)<<endl;
+	
+    return 0;
+}
