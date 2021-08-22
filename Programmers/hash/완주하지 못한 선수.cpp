1b0adf113e4004fa232b141cb2708d1933644d55
poArlim <dkfhscjsgk96@gmail.com>
2021-08-22 16:40:54 +0900

    solved August 22, 2021

--- Programmers/hash/완주하지 못한 선수.cpp
+++ Programmers/hash/완주하지 못한 선수.cpp
@@ -8,15 +8,19 @@ string solution(vector<string> participant, vector<string> completion) {
     unordered_map<string, int> participantState;
 
     for(string name : participant){
-        participantState.insert(make_pair(name, 1));
+        participantState.insert(make_pair(name, 0));
+    }
+    
+    for(string name : participant){
+        participantState[name]++;
     }
 
-    for(string name : participant){
-        participantState[name] = 0;
+    for(string name : completion){
+        participantState[name]--;
     }
 
     for(auto pair : participantState){
-        if(pair.second == 1) answer = pair.first; 
+        if(pair.second > 0) answer = pair.first;
     }
 
     return answer;
