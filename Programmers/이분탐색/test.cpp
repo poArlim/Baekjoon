f06b880518f544051dcadabfd771cb9714e51e92
poArlim <dkfhscjsgk96@gmail.com>
2021-10-01 21:32:48 +0900

    solved Oct 1, 2021

--- Programmers/이분탐색/test.cpp
+++ Programmers/이분탐색/test.cpp
@@ -1,44 +1,27 @@
 #include <string>
 #include <vector>
-#include <queue>
+#include <algorithm>
+
 using namespace std;
 
-struct compare{
-    bool operator()(pair<int, int> a, pair<int, int> b){
-        if(a.first + a.second > b.first + b.second) return true;
-        else return false;
-    }
-};
-
 long long solution(int n, vector<int> times) {
-    priority_queue<int, vector<int>, greater<int>> ready;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, compare> working;
+    sort(times.begin(), times.end());
+    long long minTime = 1;
+    long long maxTime = (long long)times[times.size() - 1] * n;
+    long long avgTime;
     long long answer = 0;
-    long long time = 0;
-    long long waiting_number = 0;
     
-    for(int t : times) ready.push(t);
-    
-    while(waiting_number < n){
-        if(!ready.empty() && (working.empty() || time + ready.top() <= working.top().first + working.top().second)){
-            working.push(make_pair(ready.top(), time + ready.top()));
-            ready.pop();
-        }
-        else {
-            time = working.top().second;
-            working.push(make_pair(working.top().first, working.top().first + working.top().second));
-            working.pop();
+    while(minTime <= maxTime){
+        avgTime = (minTime + maxTime) / 2;
+        
+        long long cnt = 0;
+        for(int time : times){
+            cnt += avgTime / time;
         }
         
-        waiting_number += 1;
+        if(cnt < n) minTime = avgTime + 1;
+        else maxTime = avgTime - 1;
     }
     
-    while(!working.empty()){
-        if(time < working.top().second) time = working.top().second;
-        working.pop();
-    }
-    
-    answer = time;
-    
-    return answer;
+    return minTime;
 }
